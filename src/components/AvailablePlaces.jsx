924a0583d238c668e884e2f62bbfc80d335650a1
wangkodok <wangkodok@gmail.com>
2024-03-14 19:20:54 +0900

    로딩 상태를 다루는 코드

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,13 +2,16 @@ import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [loading, setLoading] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
     async function fetchPlaces() {
+      setLoading(true);
       const response = await fetch("http://localhost:3000/places");
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setLoading(false);
     }
 
     fetchPlaces();
@@ -18,6 +21,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={loading}
+      loadingText="데이터를 불러오는 중입니다."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
