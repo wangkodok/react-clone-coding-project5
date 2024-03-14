f7c321f67b1067db4bd52672c8589ae126ba5e2c
wangkodok <wangkodok@gmail.com>
2024-03-14 20:00:06 +0900

    HTTP 오류 처리 코드

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,21 +1,41 @@
 import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [loading, setLoading] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setLoading(true);
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+
+      try {
+        const response = await fetch("http://localhost:3000/places1");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("오류");
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || "장소를 불러오지 못했습니다. 다시 시도해주세요.",
+        });
+      }
+
       setLoading(false);
     }
 
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="오류가 발생했습니다." message={error.message} />;
+  }
 
   return (
     <Places
