3237b298fc0f66f5ec8209ed48601dbfbb55b73f
wangkodok <wangkodok@gmail.com>
2024-03-17 20:07:00 +0900

    데이터 전송

--- src/http.js
+++ src/http.js
@@ -8,3 +8,21 @@ export async function fetchAvailablePlaces() {
 
   return resData.places;
 }
+
+export async function updateUserPlaces(places) {
+  fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    body: JSON.stringify({ places: places }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const resData = await response.json();
+
+  if (!resData.ok) {
+    throw new Error("사용자 데이터 업데이트에 실패했습니다.");
+  }
+
+  return resData.message;
+}
